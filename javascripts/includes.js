2162661861ffded12259c2b90b0193659752d75b
Chris Angelico <stcda03@moravian.edu>
2017-04-23 13:28:34 -0400

    Updated copyright year at bottom and also made header and footer seperate include files so they can be updated and have changes propagate to all pages

---
+++ javascripts/includes.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#footer_wrap').load('/includes/footer.html');
+});
+$(document).ready(function () {
+    $('#header_wrap').load('/includes/header.html');
+});
